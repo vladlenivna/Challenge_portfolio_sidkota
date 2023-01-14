1ea35a964a78ab4377800cd74fb16d40c7c647a6
Tetiana Sidko <kafidova@gmail.com>
2023-01-14 16:37:04 +0100

    fix: deprecated executable_path warning fix - Subtask 2
    Now we use the webdriver manager instead of hardcoded chromedriver binary

--- test_cases/framework.py
+++ test_cases/framework.py
@@ -1,15 +1,15 @@
 import os
 import unittest
 from selenium import webdriver
-from utils.settings import DRIVER_PATH, IMPLICITLY_WAIT
-
+from utils.settings import IMPLICITLY_WAIT
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 class Test(unittest.TestCase):
 
     @classmethod
     def setUp(self):
-        os.chmod(DRIVER_PATH, 755)
-        self.driver = webdriver.Chrome(executable_path=DRIVER_PATH)
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
         self.driver.get('https://scouts-test.futbolkolektyw.pl/en')
         self.driver.fullscreen_window()
         self.driver.implicitly_wait(IMPLICITLY_WAIT)
